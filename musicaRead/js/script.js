ac6dd058855ff4c41ccdc7b5e45d3b66a7fac81c
HudsomSantanaDev <hudsomSantanaDev@gmail.com>
2025-07-24 16:07:48 +0100

    err
    erro

--- musicaRead/js/script.js
+++ musicaRead/js/script.js
@@ -1,18 +1,12 @@
-let correntMusic = document.querySelector('button');
-
+let correntMusic = document.querySelector('audio');
+  
 // funciona
 //os icones que nao estao reagindo ao click
 //solucao:trocar os icones externos para icones internos armazenados no computador!!!!!
-document.querySelector(".botao-play").addEventListener('click', playMusic);
+/**/
+document.querySelector(".botao-play").addEventListener("click",iniciarMusica);
 
-/*document.querySelector('.botao-play').addEventListener("click",iniciarMusica);
 function iniciarMusica(){
     correntMusic.play();
-}*/
-
-function playMusic() {
-    console.log("oi");
-    correntMusic.play();
-    document.querySelector('.botao-pause').style.display = 'block';
-    document.querySelector('.botao-play').style.display="none";
 }
+
