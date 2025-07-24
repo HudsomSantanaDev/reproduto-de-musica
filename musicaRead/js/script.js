aec94a472082960e0c1c248dcb96943215183d37
HudsomSantanaDev <hudsomSantanaDev@gmail.com>
2025-07-24 17:11:33 +0100

    teste
    teste

--- musicaRead/js/script.js
+++ musicaRead/js/script.js
@@ -1,12 +1,54 @@
 let correntMusic = document.querySelector('audio');
-  
-// funciona
-//os icones que nao estao reagindo ao click
-//solucao:trocar os icones externos para icones internos armazenados no computador!!!!!
-/**/
-document.querySelector(".botao-play").addEventListener("click",iniciarMusica);
+let musicDuration = document.querySelector(".end");
 
-function iniciarMusica(){
+document.querySelector(".botao-play").addEventListener("click", iniciarMusica);
+
+document.querySelector(".botao-pause").addEventListener("click", pausarMusica);
+
+correntMusic.addEventListener('timeupdate', atualizarBarra);
+
+
+musicDuration.textContent = secondToMinutes(Math.floor(correntMusic.duration));
+
+
+
+function iniciarMusica() {
     correntMusic.play();
+    //inversão dos botoes
+    document.querySelector(".botao-pause").style.display = "block";
+    document.querySelector(".botao-play").style.display = "none";
+
+
 }
+
+function pausarMusica() {
+    correntMusic.pause();
+    //inversão dos botoes
+    document.querySelector(".botao-pause").style.display = "none";
+    document.querySelector(".botao-play").style.display = "block";
+}
+
+function atualizarBarra() {
+    let barra = document.querySelector('progress');
+
+    //calculo para saber a porcentagem do estados da musica
+    //Math.floor arredonda
+    barra.style.width = Math.floor((correntMusic.currentTime / correntMusic.duration) * 100) + '%';
+
+    let elapsedTime = document.querySelector('.start');
+    elapsedTime.textContent = secondToMinutes(Math.floor(correntMusic.currentTime));
+}
+
+function secondToMinutes(segundos) {
+    let campoMinutos = Math.floor(segundos / 60);
+    let campoSegundos = segundos % 60;
+    if (campoSegundos < 10) {
+        campoSegundos = '0' + campoSegundos;
+
+
+    }
+    return campoMinutos + ':' + campoSegundos;
+
+}
+
 
