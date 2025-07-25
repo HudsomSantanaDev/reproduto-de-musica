feecd9a6563bc0ad6e78b42a6160444d64e59f0e
HudsomSantanaDev <hudsomSantanaDev@gmail.com>
2025-07-25 17:25:04 +0100

    erro , nao renderiza o nome  do autor
    erro ao ler  nome do autor

--- musicaRead/js/script.js
+++ musicaRead/js/script.js
@@ -1,5 +1,39 @@
 let correntMusic = document.querySelector('audio');
 let musicDuration = document.querySelector(".end");
+let image=document.querySelector('img');
+let musicName=document.querySelector('.description h2');
+let artist=document.querySelector('.description i');
+let indexMusic=0;
+let musics=[
+    {
+        titulo:'Brave',artist:'Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig',src:'music/Brave - Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig.mp3',img:'img/ahmed-saeed-3oY6iqvc2sg-unsplash.jpg'
+        },
+    {
+        titulo:'Cash',artist:'Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig',src:'music/Cash - Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig (1).mp3',
+        img:'img/jesper-stechmann-nb2l7FmbSdA-unsplash.jpg',
+        }
+        ,
+    {
+        titulo:'Clocks',artist:'Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig',src:'music/Clocks - Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig.mp3',
+        img:'img/bruce-christianson-86AN3JFiBsY-unsplash.jpg',
+        }
+          ,
+    {
+        titulo:'Hanging Motionless',artist:'Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig',src:'music/Hanging Motionless - Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig.mp3',
+        img:'img/jubeo-hernandez-ZmWLGkPe1Sg-unsplash.jpg',
+        }
+         ,
+    {
+        titulo:'Numb Sky',artist:'Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig',src:'music/Numb Sky - Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig.mp3',
+        img:'img/hannah-morgan-dnkM5wPjVdg-unsplash.jpg',
+        }
+         ,
+    {
+        titulo:'Numb Sky',artist:'Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig',src:'music/Numb Sky - Mark Karan, Scott Guberman, Angeline Saris, Jeremy Hoenig.mp3',
+        img:'img/lee-jeffs-60wCqYf7Fg8-unsplash.jpg',
+        }
+];
+
 
 document.querySelector(".botao-play").addEventListener("click", iniciarMusica);
 
@@ -7,8 +41,29 @@ document.querySelector(".botao-pause").addEventListener("click", pausarMusica);
 
 correntMusic.addEventListener('timeupdate', atualizarBarra);
 
-//proximo passo Selecionar imagem
+
 musicDuration.textContent = secondToMinutes(Math.floor(correntMusic.duration));
+
+document.getElementById('anterior').addEventListener("click",()=>{
+    indexMusic--;
+renderizarMusica(indexMusic);
+
+});
+document.getElementById("proximo").addEventListener("click",()=>{
+    indexMusic++;
+renderizarMusica(indexMusic);
+});
+
+function renderizarMusica(index){
+    //erro ao ler src
+correntMusic.setAttribute('src',musics[index].src);
+correntMusic.addEventListener('loadeddata',()=>{
+    musicName.textContent=musics[index].titulo;
+    artist.textContent=musics[index].artist;
+    image.src=musics[index].img;
+    parseInt( musicDuration.textContent = secondToMinutes(Math.floor(correntMusic.duration)));
+});
+}
 
 
 
